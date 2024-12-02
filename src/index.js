5c6c1db2f08d45cc23c77291537c34b58b344add
levantu2003 <levantu18071970@gmail.com>
2024-12-02 09:50:51 +0700

    Update đăng nhập qua google

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -11,21 +12,24 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { PostAllCategory } from "./crawl";
 import UserContextProvider from "./userSide/contexts/UserContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // PostAllCategory();
 root.render(
-    <UserContextProvider>
-        <BrowserRouter>
-            <ToastContainer
-                position="top-right"
-                autoClose={3000}
-                closeOnClick
-                pauseOnHover={false}
-                theme="dark"
-            />
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </UserContextProvider>
+    <GoogleOAuthProvider clientId="1016601245951-au27qhl0fmngm6b0ke553rps0huclp1o.apps.googleusercontent.com">
+        <UserContextProvider>
+            <BrowserRouter>
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    closeOnClick
+                    pauseOnHover={false}
+                    theme="dark"
+                />
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </BrowserRouter>
+        </UserContextProvider>
+    </GoogleOAuthProvider>
 );
--- src/userSide/pages/Login.jsx
+++ src/userSide/pages/Login.jsx
@@ -7,10 +7,93 @@ import { Form, FormGroup } from "reactstrap";
 import * as Yup from "yup";
 import { userLoginApi } from "../../redux/slices/userSlice";
 import "../styles/login.css";
+import { useGoogleLogin } from '@react-oauth/google';
+import { signupServices } from "../../services/signupService";
+import { editProfileApi } from "../../redux/slices/userSlice";
 
 const Login = ({ onClose, setShowSignup, setShowLogin }) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const handleGoogleLogin = useGoogleLogin({
+        onSuccess: async (response) => {
+            try {
+                const userInfo = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+                    headers: {
+                        Authorization: `Bearer ${response.access_token}`,
+                    },
+                }).then(res => res.json());
+
+                // Tạo dữ liệu đăng nhập từ email Google
+                const loginData = {
+                    email: userInfo.email,
+                    password: "Google@" + userInfo.email
+                };
+
+                // Thử đăng nhập trước
+                const loginResponse = await dispatch(userLoginApi(loginData));
+
+                if (loginResponse.payload.status === 200) {
+                    // Nếu đăng nhập thành công, cập nhật ảnh đại diện
+                    const userData = loginResponse.payload.data[0];
+                    // Cập nhật ảnh đại diện nếu chưa có
+                    if (!userData.pathImg) {
+                        const updateData = {
+                            ...userData,
+                            pathImg: userInfo.picture // Lấy ảnh từ Google
+                        };
+                        const updateResponse = await dispatch(editProfileApi(updateData));
+                        if (updateResponse.payload.data.status === 200) {
+                            localStorage.setItem('user', JSON.stringify(updateData));
+                        }
+                    } else {
+                        localStorage.setItem('user', JSON.stringify(userData));
+                    }
+                    toast.success('Đăng nhập Google thành công!');
+                    onClose();
+                    navigate('/home');
+                    window.location.reload();
+                } else {
+                    // Nếu đăng nhập thất bại, thực hiện đăng ký
+                    const signupData = {
+                        email: userInfo.email,
+                        name: userInfo.name,
+                        password: loginData.password,
+                        phone: "0000000000",
+                        address: "Chưa cập nhật",
+                        pathImg: userInfo.picture // Thêm ảnh đại diện khi đăng ký
+                    };
+
+                    const signupResponse = await signupServices(signupData);
+
+                    if (signupResponse.status === 200) {
+                        const newLoginResponse = await dispatch(userLoginApi(loginData));
+
+                        if (newLoginResponse.payload.status === 200) {
+                            localStorage.setItem('user', JSON.stringify({
+                                ...newLoginResponse.payload.data[0],
+                                pathImg: userInfo.picture
+                            }));
+                            toast.success('Đăng ký và đăng nhập Google thành công!');
+                            onClose();
+                            navigate('/home');
+                            window.location.reload();
+                        } else {
+                            toast.error('Đăng nhập thất bại sau khi đăng ký!');
+                        }
+                    } else {
+                        toast.error('Đăng ký tài khoản thất bại!');
+                    }
+                }
+            } catch (error) {
+                toast.error('Đăng nhập Google thất bại!');
+                console.error('Error during Google login:', error);
+            }
+        },
+        onError: () => {
+            toast.error('Đăng nhập Google thất bại!');
+        }
+    });
 
     const formik = useFormik({
         initialValues: {
@@ -100,7 +183,11 @@ const Login = ({ onClose, setShowSignup, setShowLogin }) => {
                     <div className="login__divider">
                         <span>Hoặc</span>
                     </div>
-                    <button type="button" className="login__google">
+                    <button
+                        type="button"
+                        className="login__google"
+                        onClick={() => handleGoogleLogin()}
+                    >
                         <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google" />
                         Tiếp tục với Google
                     </button>
