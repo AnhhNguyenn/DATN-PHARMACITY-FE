a32850a942bfe0c5d0c969fcb3ff2ecb270f9b6d
levantu2003 <levantu18071970@gmail.com>
2024-12-11 10:35:19 +0700

    Deploy lên web

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
@@ -12,24 +11,47 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { PostAllCategory } from "./crawl";
 import UserContextProvider from "./userSide/contexts/UserContext";
+// Firebase imports moved to the top
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// PostAllCategory();
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuk2Pc_3Xh1K9CIJUx6S_E-rmQGRH_7gs",
+  authDomain: "pharmacity-clone.firebaseapp.com",
+  projectId: "pharmacity-clone",
+  storageBucket: "pharmacity-clone.firebasestorage.app",
+  messagingSenderId: "876543642305",
+  appId: "1:876543642305:web:8460d7ee3ec2f34d77e240",
+  measurementId: "G-D7NBP0X5WX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// PostAllCategory(); // Consider moving this to useEffect in App.js as suggested earlier
+
 root.render(
-    <GoogleOAuthProvider clientId="1016601245951-au27qhl0fmngm6b0ke553rps0huclp1o.apps.googleusercontent.com">
-        <UserContextProvider>
-            <BrowserRouter>
-                <ToastContainer
-                    position="top-right"
-                    autoClose={3000}
-                    closeOnClick
-                    pauseOnHover={false}
-                    theme="dark"
-                />
-                <Provider store={store}>
-                    <App />
-                </Provider>
-            </BrowserRouter>
-        </UserContextProvider>
-    </GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId="1016601245951-au27qhl0fmngm6b0ke553rps0huclp1o.apps.googleusercontent.com">
+    <UserContextProvider>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+          theme="dark"
+        />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </UserContextProvider>
+  </GoogleOAuthProvider>
 );
